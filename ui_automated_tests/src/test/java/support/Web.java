15531f9aa06731425be31f4c452b1af61dc06eb7
Marcelo Vasconcelos <marcelo.pv@samsung.com>
2019-09-25 14:45:35 -0400

    Add some arguments to webdriver

--- ui_automated_tests/src/test/java/support/Web.java
+++ ui_automated_tests/src/test/java/support/Web.java
@@ -13,10 +13,10 @@ public class Web {
         //Headless
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
-        options.setExperimentalOption("useAutomationExtension", false);
+        options.setExperimentalOption("--disable-dev-shm-usage", false);
         options.addArguments("headless");
         options.addArguments("window-size=1200x600");
-        options.setExperimentalOption("useAutomationExtension", false);
+        
 
         // To OPen browser remove options from parameter
         System.setProperty("webdriver.chrome.driver", "D:\\workspace-slave\\workspace\\AutomaticTest\\ui_automated_tests\\src\\test\\java\\driver\\chromedriver.exe");
