2bba2cfc20696654538b762c977017892bb89753
Jakelane Araujo Ribeiro <jakelane.ar@sidiamao.com.br>
2019-09-25 18:07:57 -0400

    update Web.java with chromedriver

--- ui_automated_tests/src/test/java/support/Web.java
+++ ui_automated_tests/src/test/java/support/Web.java
@@ -19,15 +19,14 @@ public class Web {
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
 		options.addArguments("--proxy-server='105.112.150.10'");
-        //options.setExperimentalOption("--disable-dev-shm-usage");
-		//options.addArguments("--headless");
+		options.addArguments("--headless");
         //options.setExperimentalOption("useAutomationExtension", false);
         //options.addArguments("window-size=1200x600");
         
 
         // To OPen browser remove options from parameter
        // System.setProperty("webdriver.chrome.driver", "D:\\workspace-slave\\workspace\\AutomaticTest\\ui_automated_tests\\src\\test\\java\\driver\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe --whitelist-ip %*");
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
         WebDriver navegador = new ChromeDriver(options);
         navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
