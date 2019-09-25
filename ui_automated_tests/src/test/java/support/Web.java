09b611dc56c86a39b735ecb5dbd0b9b054286d18
Marcelo PV <marcelopv>
2019-09-25 00:01:46 -0400

    Headless mode added and Chromedriver was put inside the project

--- ui_automated_tests/src/test/java/support/Web.java
+++ ui_automated_tests/src/test/java/support/Web.java
@@ -2,15 +2,22 @@ package support;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
 public class Web {
 
     public static WebDriver createChrome(String url) {
-        // OPen browser
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\marce\\UTIL\\drivers\\chromedriver.exe");
-        WebDriver navegador = new ChromeDriver();
+
+        //Headless
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("window-size=1200x600");
+
+        // To OPen browser remove options from parameter
+        System.setProperty("webdriver.chrome.driver", "\\driver\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver(options);
         navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         navegador.get(url);
